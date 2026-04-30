29f96d566f7cb2be942445df4b973b2a669178b1
ag270995 <32455784+ag270995@users.noreply.github.com>
2026-04-30 04:30:01 -0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = piles[0];
+        for (int pile : piles) {
+            r = Math.max(r, pile);
+        }
+        int max = r;
+        while (l <= r) {
+            int mid = l + (r - l)/2;
+            if (checkRate(mid, piles, h) < 0) {
+                l = mid + 1;
+            } else if (checkRate(mid, piles, h) >= 0) {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+
+    int checkRate(int rate, int[] piles, int h) {
+        long hours = 0;
+        for (int pile : piles) {
+           hours += Math.ceil((double) pile / rate);  // slow, floating point
+        }
+        if (hours == h) {
+            return 0;
+        } else if (hours < h) {
+            return 1;
+        }
+        return -1;       
+    }
+}
