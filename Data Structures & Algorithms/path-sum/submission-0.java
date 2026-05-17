9ec790f57deee286134e5ad50cb6f6fb3d429c02
ag270995 <32455784+ag270995@users.noreply.github.com>
2026-05-17 06:55:08 -0700

    Add: path-sum - submission-0

---
+++ Data Structures & Algorithms/path-sum/submission-0.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return hasPathSum(root, targetSum, 0);
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum, int sum) {
+        if (root == null) {
+            return false;
+        }
+        sum += root.val;
+        if (root.left == null && root.right == null) {
+            if (sum == targetSum) {
+                return true;
+            }
+            return false;
+        }
+        
+        if (hasPathSum(root.left, targetSum, sum)) {
+            return true;
+        }
+        if (hasPathSum(root.right, targetSum, sum)) {
+            return true;
+        }
+        return false;
+    }
+}
