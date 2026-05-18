ca83ffab0e6722f6ca1287a231cf5d9c5279ee7a
ag270995 <32455784+ag270995@users.noreply.github.com>
2026-05-18 03:46:19 -0700

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        combine(1, n, k, new ArrayList<>(), res);
+        return res;
+    }
+
+    public void combine(int i, int n, int k, List<Integer> curr, List<List<Integer>> res) {
+        
+        if (curr.size() == k) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        if (i > n) {
+            return;
+        }
+        curr.add(i);
+        combine(i + 1, n, k, curr, res);
+        curr.remove((int)curr.size() - 1);
+        combine(i + 1, n, k, curr, res);
+        
+    }
+}
