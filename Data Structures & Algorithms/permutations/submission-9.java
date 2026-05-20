29d2d1dbe8f7522487edf1221cab5f6f555315b0
ag270995 <32455784+ag270995@users.noreply.github.com>
2026-05-20 04:47:20 -0700

    Add: permutations - submission-9

---
+++ Data Structures & Algorithms/permutations/submission-9.java
@@ -0,0 +1,26 @@
+class Solution {
+    Set<Integer> set = new HashSet<>();
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        permute(nums, new ArrayList<>());
+        return res;
+    }
+
+    public void permute(int[] nums, List<Integer> curr) {
+        int n = nums.length;
+        if (curr.size() == n) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (set.contains(nums[i])) {
+                continue;
+            }
+            set.add(nums[i]);
+            curr.add(nums[i]);
+            permute(nums, curr);
+            set.remove(nums[i]);
+            curr.removeLast();
+        }
+    }
+}
