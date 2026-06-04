2cff0711c5d48a8c6a34898fbe7ee83739868cec
ag270995 <32455784+ag270995@users.noreply.github.com>
2026-06-04 04:21:19 -0700

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+
+        int[] degree = new int[n + 1];
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+
+            degree[u]++;
+            degree[v]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (degree[i] == 1) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+
+            degree[node]--; // mark this leaf as removed
+
+            for (int nei : adj.get(node)) {
+                degree[nei]--;
+
+                if (degree[nei] == 1) {
+                    queue.offer(nei);
+                }
+            }
+        }
+
+        for (int i = edges.length - 1; i >= 0; i--) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            if (degree[u] > 0 && degree[v] > 0) {
+                return new int[]{u, v};
+            }
+        }
+
+        return new int[0];
+    }
+}
