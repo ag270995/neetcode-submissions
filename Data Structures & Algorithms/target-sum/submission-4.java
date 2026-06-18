bd33240b93615e8899d4339743ad20c88b7a5862
ag270995 <32455784+ag270995@users.noreply.github.com>
2026-06-18 16:59:45 +0530

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.java
@@ -0,0 +1,33 @@
+class Solution {
+    Map<Integer, Map<Integer, Integer>> memo = new HashMap<>();
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        memo.put(n, new HashMap<>());
+        memo.get(n).put(target, 1);
+        return dfs(nums, target, 0, 0);
+    }
+
+    public int dfs(int[] nums, int target, int sum, int i) {
+        if (memo.containsKey(i) &&  memo.get(i).containsKey(sum)) {
+            memo.get(i).get(sum);
+        }
+        int n = nums.length;
+        
+        if (i == n) {
+            if (sum == target) {
+                return 1;
+            }
+            memo.get(n).put(sum, 0);
+            return 0;
+        }
+        int res =  dfs(nums, target, sum + nums[i], i + 1) 
+                + dfs(nums, target, sum - nums[i], i + 1);
+        if (!memo.containsKey(i)) {
+            memo.put(i, new HashMap<>());
+        }
+        memo.get(i).put(sum, res);
+        
+        return res;
+        
+    }
+}
