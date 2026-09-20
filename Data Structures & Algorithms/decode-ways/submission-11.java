213bfe6d954db0de93f14a81e5da19dbb973a73c
ag270995 <32455784+ag270995@users.noreply.github.com>
2026-09-20 15:52:50 +0530

    Add: decode-ways - submission-11

---
+++ Data Structures & Algorithms/decode-ways/submission-11.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= n; i++) {//iterating on length
+            char curr = s.charAt(i - 1);
+            char prev = s.charAt(i - 2);
+            if (curr != '0') {
+                dp[i] += dp[i - 1];
+            }
+            if (prev == '1' || (prev == '2' && curr < '7' )) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+        
+    }
+}
