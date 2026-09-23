86e4b259609b0f70a387236919a45ec6a52df52c
ag270995 <32455784+ag270995@users.noreply.github.com>
2026-09-23 19:57:01 +0530

    Add: target-sum - submission-35

---
+++ Data Structures & Algorithms/target-sum/submission-35.java
@@ -0,0 +1,49 @@
+class Solution {
+    Integer[][] memo;
+    int totalSum = 0;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        // The current sum can range from -totalSum to +totalSum.
+        // Therefore, the number of possible sums is:
+        // +totalSum - (-totalSum) + 1 = 2 * totalSum + 1.
+        //
+        // We use totalSum as an offset to convert negative sums
+        // into valid array indices:
+        //   sum = -totalSum -> index -> 0
+        //   sum = 0         -> index -> totalSum
+        //   sum = +totalSum -> index -> 2 * totalSum
+        //
+        // memo[sum + totalSum][i] stores the number of ways to
+        // reach 'target' using elements from index i onward,
+        // given that the current sum is 'sum'.
+        memo = new Integer[(2 * totalSum) + 1][nums.length + 1];
+
+        return findTargetSumWays(nums, target, 0, 0);
+    }
+
+    public int findTargetSumWays(int[] nums, int target, int i, int sum) {
+        // All numbers have been processed.
+        // A valid way exists if the current sum equals target.
+        if (i == nums.length) {
+            return target == sum ? 1 : 0;
+        }
+
+        int sumIdx = totalSum + sum;
+
+        if (memo[sumIdx][i] != null) {
+            return memo[sumIdx][i];
+        }
+
+        // For the current number, we have two choices:
+        // 1. Assign '-' -> sum - nums[i]
+        // 2. Assign '+' -> sum + nums[i]
+        memo[sumIdx][i] = findTargetSumWays(nums, target, i + 1, sum - nums[i])
+            + findTargetSumWays(nums, target, i + 1, sum + nums[i]);
+
+        return memo[sumIdx][i];
+    }
+}
